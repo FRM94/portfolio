a85602a52de4a5789070d8ac0db8c9af3342dbeb
Fabian <fabian@competa.com>
2018-01-05 11:48:12 +0100

    Added dynamic image loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,9 @@
 const webpack = require('webpack');
+
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const autoprefixer = require('autoprefixer');
+
 const path = require('path');
 
 module.exports = {
@@ -10,25 +13,50 @@ module.exports = {
 		filename: 'app.bundle.js'
 	},
 	module: {
-		rules: [{
-			test: /\.scss$/,
-			use: ExtractTextPlugin.extract({
-				fallback: 'style-loader',
+		rules: [
+			{
+				test: /\.scss$/,
+				use: ExtractTextPlugin.extract({
+					use: [
+						{
+							loader: 'css-loader',
+							options: {
+								publicPath: '/dist'
+							}
+						},
+						{
+							loader: 'postcss-loader',
+							options: {
+								plugins: () => {
+									return [
+										require('autoprefixer')()
+									];
+								},
+								publicPath: '/dist'
+							}
+						},
+						{
+							loader: 'sass-loader',
+							options: {
+								publicPath: '/dist'
+							}
+						}
+					]
+				})
+			},
+			{
+				test: /\.(jpe?g|png|gif|svg)$/i,
 				use: [
-					'css-loader',
-					'sass-loader'
-				],
-				publicPath: '/dist'
-			}),
-			// test: /\.(jpg|png)$/,
-			// use: [{
-			// 	loader: 'file-loader',
-			// 	options: {
-			// 		name: '[name].[ext]',
-			// 		outputPath: '/dist/images/',
-			// 	}
-			// }]
-		}]
+					{
+						loader: 'file-loader',
+						options: {
+							name: '[name].[ext]',
+							outputPath: 'images/',
+						}
+					}
+				]
+			}
+		]
 	},
 	plugins: [
 		new HtmlWebpackPlugin({

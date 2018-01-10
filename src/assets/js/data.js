4f2418898e6047df3ff5a81b4659bd63a87f5899
Fabian <fabian@competa.com>
2018-01-10 17:23:04 +0100

    Fluid animations

--- src/assets/js/data.js
+++ src/assets/js/data.js
@@ -2,7 +2,7 @@
 
 import * as base from './base.js';
 
-function loadContent() {
+function loadPreview() {
 	var dataRequest = new XMLHttpRequest();
 	dataRequest.onreadystatechange = function () {
 		if (this.readyState == 4 && this.status == 200) {
@@ -25,36 +25,6 @@ function loadContent() {
 					}
 				}
 				insertTitle();
-
-				// Dynamically add category tags
-				var tags = data[i].tags;
-				function createList() {
-					if (data[i].hasOwnProperty('tags')) {
-						var list = document.createElement('ul');
-						list.setAttribute('class', 'section__tags');
-						for (var x = 0; x < tags.length; x++) {
-							var item = document.createElement('li');
-							item.setAttribute('class', 'tag');
-							item.textContent = tags[x];
-							list.appendChild(item);
-						}
-						target.appendChild(list);
-						target.insertBefore(list, target.childNodes[1]);
-					}
-				}
-				createList();
-
-				// Dynamically add category description
-				function insertDescription() {
-					if (data[i].hasOwnProperty('description')) {
-						var description = document.createElement('p');
-						description.textContent = data[i].description;
-						description.setAttribute('class', 'section__text');
-						target.appendChild(description);
-						target.insertBefore(description, target.childNodes[2]);
-					}
-				}
-				insertDescription();
 
 				// Dynamically add picture overlay (to all)
 				function insertOverlay() {
@@ -81,5 +51,116 @@ function loadContent() {
 	dataRequest.open('GET', '/src/assets/data/categories.json');
 	dataRequest.send();
 }
+loadPreview();
 
-loadContent();
+function loadFullCategory() {
+	var dataRequest = new XMLHttpRequest();
+	dataRequest.onreadystatechange = function () {
+		if (this.readyState == 4 && this.status == 200) {
+			var data = JSON.parse(dataRequest.responseText);
+
+			//Dynamically add category tags
+			var tags = data[i].tags;
+			function createList() {
+				if (data[i].hasOwnProperty('tags')) {
+					var list = document.createElement('ul');
+					list.setAttribute('class', 'section__tags');
+					for (var x = 0; x < tags.length; x++) {
+						var item = document.createElement('li');
+						item.setAttribute('class', 'tag');
+						item.textContent = tags[x];
+						list.appendChild(item);
+					}
+					target.appendChild(list);
+					target.insertBefore(list, target.childNodes[1]);
+				}
+			}
+			createList();
+
+		}
+	};
+	dataRequest.open('GET', '/src/assets/data/categories.json');
+	dataRequest.send();
+}
+
+// function loadContent() {
+// 	var dataRequest = new XMLHttpRequest();
+// 	dataRequest.onreadystatechange = function () {
+// 		if (this.readyState == 4 && this.status == 200) {
+// 			var data = JSON.parse(dataRequest.responseText);
+// 			var category = document.getElementsByClassName('section');
+
+// 			for (var i = 0; i < category.length; i++) {
+// 				// Dynamically add category classes
+// 				var target = category[i];
+// 				target.setAttribute('class', 'section ' + data[i].category);
+
+// 				// Dynamically add category titles
+// 				function insertTitle() {
+// 					if (data[i].hasOwnProperty('name')) {
+// 						var title = document.createElement('h2');
+// 						title.textContent = data[i].name;
+// 						title.setAttribute('class', 'section__title');
+// 						target.appendChild(title);
+// 						target.insertBefore(title, target.childNodes[0]);
+// 					}
+// 				}
+// 				insertTitle();
+
+// 				// Dynamically add category tags
+// 				var tags = data[i].tags;
+// 				function createList() {
+// 					if (data[i].hasOwnProperty('tags')) {
+// 						var list = document.createElement('ul');
+// 						list.setAttribute('class', 'section__tags');
+// 						for (var x = 0; x < tags.length; x++) {
+// 							var item = document.createElement('li');
+// 							item.setAttribute('class', 'tag');
+// 							item.textContent = tags[x];
+// 							list.appendChild(item);
+// 						}
+// 						target.appendChild(list);
+// 						target.insertBefore(list, target.childNodes[1]);
+// 					}
+// 				}
+// 				createList();
+
+// 				// Dynamically add category description
+// 				function insertDescription() {
+// 					if (data[i].hasOwnProperty('description')) {
+// 						var description = document.createElement('p');
+// 						description.textContent = data[i].description;
+// 						description.setAttribute('class', 'section__text');
+// 						target.appendChild(description);
+// 						target.insertBefore(description, target.childNodes[2]);
+// 					}
+// 				}
+// 				insertDescription();
+
+// 				// Dynamically add picture overlay (to all)
+// 				function insertOverlay() {
+// 					var overlay = document.createElement('div');
+// 					overlay.setAttribute('class', 'section__overlay');
+// 					target.appendChild(overlay);
+// 					target.insertBefore(overlay, target.childNodes[3]);
+// 				}
+// 				insertOverlay();
+
+// 				// Dynamically add category picture (to all)
+// 				function insertBackground() {
+// 					var picture = document.createElement('img');
+// 					picture.setAttribute('class', 'section__background');
+// 					picture.src = 'dist/images/' + data[i].category + '-bg.jpg';
+// 					target.appendChild(picture);
+// 					target.insertBefore(picture, target.childNodes[4]);
+// 				}
+// 				insertBackground();
+
+// 			}
+// 		}
+// 	};
+// 	dataRequest.open('GET', '/src/assets/data/categories.json');
+// 	dataRequest.send();
+// }
+
+//loadContent();
--- src/assets/js/transitions.js
+++ src/assets/js/transitions.js
@@ -1,22 +1,47 @@
 'use: strict';
 
 function toggleSections() {
-	var parent = document.querySelectorAll('.container');
+	var parent = document.querySelector('.container');
 	var items = document.querySelectorAll('.section');
 
 	for (var i = 0; i < items.length; i++) {
 		items[i].addEventListener('click', function () {
+			// Item closed (no new item opened)
 			if (this.classList.contains('active')) {
-				for (var x = 0; x < items.length; x++) {
-					items[x].classList.remove('active');
+				this.classList.remove('active');
+				parent.classList.remove('active');
+				if (this.parentElement.classList.contains('stacked')) {
+					this.parentElement.classList.remove('active');
 				}
-				parent[0].classList.remove('active');
 			} else {
 				for (var x = 0; x < items.length; x++) {
-					items[x].classList.remove('active');
+					// Switched to another item
+					if (items[x].classList.contains('active')) {
+						/* TODO - Toggle local data
+
+							1. Hide current data
+							2. Run items animations
+							3. Show loaded data
+						*/
+						items[x].classList.remove('active');
+						if (items[x].parentElement.classList.contains('stacked')) {
+							items[x].parentElement.classList.remove('active');
+						}
+					}
 				}
-				parent[0].classList.add('active');
+				// Item opened
+				parent.classList.add('active');
+				if (this.parentElement.classList.contains('stacked')) {
+					this.parentElement.classList.add('active');
+				}
 				this.classList.add('active');
+				loadFullCategory();
+				/* TODO - Load server data
+
+					1. Hide current data
+					2. Load item data
+					3. Create elements and assign data to it
+				*/
 			}
 		});
 	}

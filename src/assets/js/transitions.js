04a11ab15a94850741227e4d74357861bee243b4
Fabian <fabian@competa.com>
2018-01-18 09:31:12 +0100

    Fixed transition and closing bugs

--- src/assets/js/transitions.js
+++ src/assets/js/transitions.js
@@ -11,13 +11,11 @@ function toggleSections() {
 	for (let i = 0; i < items.length; i++) {
 		items[i].addEventListener('click', function () {
 			for (let x = 0; x < items.length; x++) {
-				// Item opened
 				parent.classList.add('active');
 				if (this.parentElement.classList.contains('stacked')) {
 					this.parentElement.classList.add('active');
 					parent.classList.add('no-direct');
 				}
-				// Switched
 				if (!this.classList.contains('active')) {
 					this.classList.add('active');
 					let content = items[i].querySelector('.section__content');
@@ -25,7 +23,9 @@ function toggleSections() {
 				}
 				if (items[x].classList.contains('active') && items[x] !== this) {
 					items[x].classList.remove('active');
-					if (items[x].parentElement.classList.contains('stacked')) {
+					if (items[x].parentElement.classList.contains('stacked') && this.parentElement.classList.contains('stacked')) {
+						// Do nothing
+					} else {
 						items[x].parentElement.classList.remove('active');
 						parent.classList.remove('no-direct');
 					}
@@ -35,12 +35,14 @@ function toggleSections() {
 		let exit = items[i].querySelector('.content__close');
 		exit.addEventListener('click', function (e) {
 			e.stopPropagation();
+
 			items[i].classList.remove('active');
 			parent.classList.remove('active');
 			if (items[i].parentElement.classList.contains('stacked')) {
 				items[i].parentElement.classList.remove('active');
 				parent.classList.remove('no-direct');
 			}
+
 			let content = items[i].querySelector('.section__content');
 			setTimeout(function () {
 				scrollToTop(content);
@@ -49,3 +51,5 @@ function toggleSections() {
 	}
 }
 toggleSections();
+
+//&& items[x] !== this
